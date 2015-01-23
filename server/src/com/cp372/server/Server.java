540f3c977813e1b8599e639eb035554cb39c368f
novon229 <novon229@gmail.com>
2015-01-23 12:40:56 -0500

    while loop server testing

--- server/src/com/cp372/server/Server.java
+++ server/src/com/cp372/server/Server.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.cp372.server.models.Point;
 import com.cp372.server.models.Shape;
@@ -15,24 +16,44 @@ public final class Server {
 		int port = 5555;// new Integer(argv[0]).intValue();
 		// System.out.println(sss.charAt(0));
 		// Establish the listen socket.
-		ServerSocket socket = new ServerSocket(port);
+//		ServerSocket socket = new ServerSocket(port);
 
 		System.out.println("Preparing to launch the web server...");
-		
+	
 		// Try 4 points
 		List<Point> points = new ArrayList<Point>();
 		
-		points.add(new Point(1, 0));
-		points.add(new Point(0, 1));
-		points.add(new Point(1, 1));		
-		points.add(new Point(0, 0));
-		
-		
-		Shape s = ShapeFactory.createShape(points);
-		s.printPoints();
-		
+		while (true){
+			Scanner scan = new Scanner(System.in);
+			float a;
+			float b;
+			System.out.println("Point 1 x");
+			a = scan.nextFloat();
+			System.out.println("point 1 y");
+			b = scan.nextFloat();
+			points.add(new Point(a, b));
+			System.out.println("point 2 x");
+			a = scan.nextFloat();
+			System.out.println("point 2 y");
+			b = scan.nextFloat();
+			points.add(new Point(a, b));
+			System.out.println("point 3 x");
+			a = scan.nextFloat();
+			System.out.println("point 3 y");
+			b = scan.nextFloat();
+			points.add(new Point(a, b));
+			System.out.println("point 4 x");
+			a = scan.nextFloat();
+			System.out.println("point 4 y");
+			b = scan.nextFloat();
+			points.add(new Point(a, b));		
+			System.out.println("next");
+			Shape s = ShapeFactory.createShape(points);
+			s.printPoints();
+			points.clear();
+		}
 		// Process HTTP service requests in an infinite loop.
-		while (true) {
+	/*	while (true) {
 			// Listen for a TCP connection request.
 			Socket connection = socket.accept();
 
@@ -45,7 +66,7 @@ public final class Server {
 			// Start the thread.
 			thread.start();
 		}
-		
+		*/
 		// close socket if we ever manage to leave the loop
 		// socket.close();
 	}

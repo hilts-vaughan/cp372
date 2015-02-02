a700cf334f5930d8b12144a80a3c91ce8ded52d7
Vaughan Hilts <hilts.vaughan@gmail.com>
2015-02-02 11:54:47 -0500

    Another whitespace trim fix

--- client/src/client/Shape.java
+++ client/src/client/Shape.java
@@ -21,8 +21,8 @@ public class Shape {
 		// Convert pieces here
 		for(int i = 0; i < pointPieces.length; i+= 2) {
 			
-			String pointPiece = pointPieces[i];
-			String pointPiece2 = pointPieces[i + 1];
+			String pointPiece = pointPieces[i].trim();
+			String pointPiece2 = pointPieces[i + 1].trim();
 			
 			int x = Integer.parseInt(pointPiece.trim());
 			int y = Integer.parseInt(pointPiece2.trim());
